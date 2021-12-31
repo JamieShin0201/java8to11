73edf17f50f1bde4e427709b2c0b1a435e18bf91
JamieShin0201 <jamieshin1450@gmail.com>
2021-12-31 22:01:14 +0900

    Default method in List

--- src/jamie/defaultmethod/App.java
+++ src/jamie/defaultmethod/App.java
@@ -1,5 +1,9 @@
 package jamie.defaultmethod;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Spliterator;
+
 public class App {
 
     public static void main(String[] args) {
@@ -7,5 +11,19 @@ public class App {
         foo.printName();
         foo.printNameUppercase();
         Foo.print();
+
+        List<String> names = new ArrayList<>();
+        names.add("Jamie");
+        names.add("Sunny");
+        names.add("Suk");
+        names.add("Ha");
+
+        names.forEach(System.out::println);
+        System.out.println();
+
+        Spliterator<String> spliterator1 = names.spliterator();
+        Spliterator<String> spliterator2 = spliterator1.trySplit();
+        while (spliterator1.tryAdvance(System.out::println)) ;
+        while (spliterator2.tryAdvance(System.out::println)) ;
     }
 }
